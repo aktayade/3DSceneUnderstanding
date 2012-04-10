01e0d3ebcf5d2465656fa2033958cb5c792a31b2
Srinath Sridhar <srinath@ia.(none)>
2012-04-10 19:56:00 -0400

    Added a pcl directory that contains PCL code for point cloud visualization, converting old binary PCD files to ASCII PCD and also some code for feature extraction.

---
+++ pcl/test/build/src/CMakeFiles/spinextract.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/spinextract.dir/pcd_spin_image.cpp.o"
+  "spinextract.pdb"
+  "spinextract"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/spinextract.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
