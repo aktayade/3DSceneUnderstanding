9385bc2d0d12d6c7a10dc50bd74a303eceafc57d
Srinath Sridhar <srinath@ia.(none)>
2012-04-10 23:55:24 -0400

    Successfully done SIFT 3D keypoint extraction.

--- pcl/spinextract/src/main.cpp
+++ pcl/spinextract/src/main.cpp
@@ -5,50 +5,92 @@
 #include <pcl/point_types.h>
 #include <pcl/features/normal_3d.h>
 #include <pcl/features/spin_image.h>
+#include <pcl/keypoints/sift_keypoint.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/visualization/cloud_viewer.h>
 
-int
-main (int, char** argv)
+using namespace std;
+using namespace pcl;
+
+// Constants from http://pastebin.com/8s5k8M1A
+const float min_scale = 0.01;
+const int nr_octaves = 3;
+const int nr_scales_per_octave = 3;
+const float min_contrast = 10.0;
+
+int main (int, char ** argv)
 {
-  std::string filename = argv[1];
-  std::cout << "Reading " << filename << std::endl;
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+  // Load the point cloud data
+  PointCloud<PointXYZRGB>::Ptr cloud(new PointCloud<PointXYZRGB>);
+  PointCloud<PointWithScale>::Ptr siftcloud(new PointCloud<PointWithScale>);
 
-  if (pcl::io::loadPCDFile <pcl::PointXYZ> (filename.c_str (), *cloud) == -1)
-  // load the file
+  if (pcl::io::loadPCDFile<PointXYZRGB>(argv[1], *cloud) == -1)
   {
     PCL_ERROR ("Couldn't read file");
-    return (-1);
+    return -1;
   }
-  std::cout << "Loaded " << cloud->points.size () << " points." << std::endl;
+  cout << "Loaded " << cloud->points.size () << " points." << endl;
 
-  // Compute the normals
-  pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> normal_estimation;
-  normal_estimation.setInputCloud (cloud);
+  // STEP 1: Detect SIFT 3D Keypoints in the point cloud
+  SIFTKeypoint<PointXYZRGB, PointWithScale > SIFTPoints;
 
-  pcl::search::KdTree<pcl::PointXYZ>::Ptr kdtree (new pcl::search::KdTree<pcl::PointXYZ>);
-  normal_estimation.setSearchMethod (kdtree);
+  // Use a non-FLANN-based KdTree to perform neighborhood searches
+  pcl::search::KdTree<pcl::PointXYZRGB>::Ptr kdtree (new pcl::search::KdTree<pcl::PointXYZRGB>);
+  SIFTPoints.setSearchMethod(kdtree);
 
-  pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud< pcl::Normal>);
-  normal_estimation.setRadiusSearch (0.03);
-  normal_estimation.compute (*normals);
+  // Set the detection parameters
+  SIFTPoints.setScales(min_scale, nr_octaves, nr_scales_per_octave);
+  SIFTPoints.setMinimumContrast(min_contrast);
 
-  // Setup spin image computation
-  pcl::SpinImageEstimation<pcl::PointXYZ, pcl::Normal, pcl::Histogram<153> > spin_image_descriptor(8, 0.5, 16);
-  spin_image_descriptor.setInputCloud (cloud);
-  spin_image_descriptor.setInputNormals (normals);
+  SIFTPoints.setInputCloud(cloud);
+  SIFTPoints.compute(*siftcloud);
 
-  // Use the same KdTree from the normal estimation
-  spin_image_descriptor.setSearchMethod (kdtree);
-  pcl::PointCloud<pcl::Histogram<153> >::Ptr spin_images (new pcl::PointCloud<pcl::Histogram<153> >);
-  spin_image_descriptor.setRadiusSearch (0.2);
+  pcl::visualization::PCLVisualizer viewer("Simple Cloud Viewer");
+  pcl::visualization::PointCloudGeometryHandlerXYZ< PointWithScale > geometry(siftcloud); 
+  viewer.addPointCloud(cloud, "cloud");
+  viewer.addPointCloud(siftcloud, geometry, "keypoints");
+  viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 7, "keypoints");
+  viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, 1.0, 0.0, 0.0, "keypoints");
+  viewer.spin();
 
-  // Actually compute the spin images
-  spin_image_descriptor.compute (*spin_images);
-  std::cout << "SI output points.size (): " << spin_images->points.size () << std::endl;
+  cout << "Size of siftcloud is " << siftcloud->points.size() << endl;
 
-  // Display and retrieve the spin image descriptor vector for the first point.
-  pcl::Histogram<153> first_descriptor = spin_images->points[0];
-  std::cout << first_descriptor << std::endl;
+  // viewer.showCloud(siftcloud);
+  while(!viewer.wasStopped())
+    {
+
+    }
+
+
+
+  // // Compute the normals
+  // pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> normal_estimation;
+  // normal_estimation.setInputCloud (cloud);
+
+  // pcl::search::KdTree<pcl::PointXYZ>::Ptr kdtree (new pcl::search::KdTree<pcl::PointXYZ>);
+  // normal_estimation.setSearchMethod (kdtree);
+
+  // pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud< pcl::Normal>);
+  // normal_estimation.setRadiusSearch(2.0);
+  // normal_estimation.compute (*normals);
+
+  // // Setup spin image computation
+  // pcl::SpinImageEstimation<pcl::PointXYZ, pcl::Normal, pcl::Histogram<153> > spin_image_descriptor(8, 0.5, 16);
+  // spin_image_descriptor.setInputCloud (cloud);
+  // spin_image_descriptor.setInputNormals (normals);
+
+  // // Use the same KdTree from the normal estimation
+  // spin_image_descriptor.setSearchMethod (kdtree);
+  // pcl::PointCloud<pcl::Histogram<153> >::Ptr spin_images (new pcl::PointCloud<pcl::Histogram<153> >);
+  // spin_image_descriptor.setRadiusSearch(2.0);
+
+  // // Actually compute the spin images
+  // spin_image_descriptor.compute (*spin_images);
+  // std::cout << "SI output points.size (): " << spin_images->points.size () << std::endl;
+
+  // // Display and retrieve the spin image descriptor vector for the first point.
+  // pcl::Histogram<153> first_descriptor = spin_images->points[0];
+  // cout << first_descriptor[1] << endl;
 
   return 0;
 }
