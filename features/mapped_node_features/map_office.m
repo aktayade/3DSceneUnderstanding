b341dd886f3a38a8160d270158e77ad89e704fb0
Yu-Wei Chao <ywchao@ywchao-VirtualBox.(none)>
2012-04-22 17:35:58 -0400

    label converted corrected

--- features/mapped_node_features/map_office.m
+++ features/mapped_node_features/map_office.m
@@ -3,12 +3,12 @@ load 'office_mapping.mat'
 data = zeros(0,size(office,2));
 dput = 0;
 for ii = 1:length(office)
-    indx = find(office(ii,3) == office_mapping);
+    indx = find(office(ii,3) == office_mapping(:,1));
     if indx
         dput = dput + 1;
         data(dput, :) = office(ii,:);
-        data(dput, 3) = indx;
-        if data(dput,3) == length(office_mapping)
+        data(dput, 3) = office_mapping(indx, 2);
+        if data(dput,3) == max(office_mapping(:,2))
             data(dput,3) = 0;
         end
     end
